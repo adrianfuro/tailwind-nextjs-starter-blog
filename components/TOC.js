00516a22167e725ac395d054ec4aec81afb026a2
Adrian Furo <30745813+adrianfuro@users.noreply.github.com>
2023-07-21 17:03:21 +0300

    Fixed TOC live highlight

--- components/TOC.js
+++ components/TOC.js
@@ -1,51 +1,45 @@
-import { useState, useEffect } from 'react'
-import { useRouter } from 'next/router'
+import { useEffect, useState, useRef } from 'react'
 
 const Table_of_Contents = ({ ids }) => {
-  const router = useRouter()
-  const [currentSection, setCurrentSection] = useState('')
-  const offset = 200 // Adjust this value to your needs
+  const [activeId, setActiveId] = useState('')
+  const observer = useRef(null)
+
+  useEffect(() => {
+    const options = {
+      rootMargin: '-50px 0px 0px 0px',
+      threshold: 0.3,
+    }
+
+    observer.current = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setActiveId(entry.target.id)
+        }
+      })
+    }, options)
+
+    ids.forEach((id) => {
+      const target = document.getElementById(id.id)
+      if (target) observer.current.observe(target)
+    })
+
+    return () => {
+      ids.forEach((id) => {
+        const target = document.getElementById(id.id)
+        if (target) observer.current.unobserve(target)
+      })
+    }
+  }, [ids])
 
   const handleClick = (id) => (event) => {
     event.preventDefault()
+    const offset = 100 // adjust this to your needs
     const section = document.getElementById(id)
     window.scrollTo({
-      top: section.offsetTop - 100,
+      top: section.offsetTop - offset,
       behavior: 'smooth',
     })
   }
-
-  useEffect(() => {
-    // Check if window is defined to avoid issues with server-side rendering
-    if (typeof window !== 'undefined') {
-      const handleRouteChange = (url) => {
-        const hash = url.split('#')[1]
-        setCurrentSection(hash)
-      }
-
-      const handleScroll = () => {
-        let current = ''
-
-        ids.forEach(({ id }) => {
-          const section = document.getElementById(id)
-          const rect = section.getBoundingClientRect()
-          if (rect.top <= offset) {
-            current = id
-          }
-        })
-
-        setCurrentSection(current)
-      }
-
-      router.events.on('hashChangeComplete', handleRouteChange)
-      window.addEventListener('scroll', handleScroll)
-
-      return () => {
-        router.events.off('hashChangeComplete', handleRouteChange)
-        window.removeEventListener('scroll', handleScroll)
-      }
-    }
-  }, [])
 
   return (
     <div className="table-of-contents mt-8">
@@ -54,9 +48,9 @@ const Table_of_Contents = ({ ids }) => {
           <li className="py-4">
             <a
               href={`#${id}`}
-              onClick={handleClick(id)}
+              onClick={handleClick(id)} // added onClick handler
               className={
-                currentSection === id
+                activeId === id
                   ? 'toc-a text-primary-600 hover:font-bold dark:text-primary-400 py-4'
                   : 'toc-a hover:font-bold py-4'
               }
