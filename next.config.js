13813d269f0f3f92323a3e0de384f67ed98d4357
Adrian Furo <30745813+adrianfuro@users.noreply.github.com>
2023-08-02 16:47:47 +0300

    fixed google ads

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app https://plausible.adrianfuro.com/ https://www.googletagmanager.com/ https://pagead2.googlesyndication.com/;
-  style-src 'self' 'unsafe-inline' *.googleapis.com cdn.jsdelivr.net *.googlesyndication.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app https://plausible.adrianfuro.com/ https://www.googletagmanager.com/ *.googlesyndication.com;
+  style-src 'self' 'unsafe-inline' *.googleapis.com cdn.jsdelivr.net;
   img-src * blob: data:;
   media-src 'none';
   connect-src *;
